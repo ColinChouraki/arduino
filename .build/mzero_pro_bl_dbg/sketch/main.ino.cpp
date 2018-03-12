82703e01397977fec7ffaaa11991194806559251
ColinChouraki <colinchouraki1@gmail.com>
2018-03-12 18:15:24 +0100

    TP arduino terminé

---
+++ .build/mzero_pro_bl_dbg/sketch/main.ino.cpp
@@ -0,0 +1,29 @@
+#line 1 "c:\\Users\\eleve\\Desktop\\Marion2\\arduino\\main.ino"
+#line 1 "c:\\Users\\eleve\\Desktop\\Marion2\\arduino\\main.ino"
+//  Copyright (c) 2018 Antoine Tran Tan
+//
+
+#include "Arduino.h"
+
+// the setup function runs once when you press reset or power the board
+#line 7 "c:\\Users\\eleve\\Desktop\\Marion2\\arduino\\main.ino"
+void setup();
+#line 13 "c:\\Users\\eleve\\Desktop\\Marion2\\arduino\\main.ino"
+void loop();
+#line 7 "c:\\Users\\eleve\\Desktop\\Marion2\\arduino\\main.ino"
+void setup()
+{
+    pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+    static int potPosition;
+    potPosition=analogRead(A0);
+    digitalWrite(13,HIGH);
+    delay(potPosition);
+    digitalWrite(13,LOW);
+    delay(potPosition);
+}
+
